495b0cb4ed5dcad5952be3235530bc5d72cd755a
Lipenko-Andrei <Lipenko3005andrei@gmail.com>
2022-10-12 18:57:17 +0400

    Добавьте файлы проекта.

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Test
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool IsMaximazed = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximazed)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1000;
+                    this.Height = 720;
+                    IsMaximazed = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximazed = true;
+                }
+            }
+        }
+    }
+}
